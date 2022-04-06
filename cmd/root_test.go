3fd8636ae0930f619cb37135a71b1d78fadcd679
Jan Ritter <git@janrtr.de>
2022-04-06 20:06:56 +0200

    tests: added unit tests

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"encoding/csv"
 	"testing"
+
+	"os"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -18,4 +21,152 @@ func TestGetNameFromFilename(t *testing.T) {
 	result := getNameFromFilename(test)
 
 	assert.Equal(t, "test1", result)
+}
+
+func TestReadCSVFile(t *testing.T) {
+	filename := "../tests/short.csv"
+	result := readCSVFile(filename)
+
+	assert.Equal(t, [][]string{
+		{
+			"metric_name",
+			"timestamp",
+			"metric_value",
+			"check",
+			"error",
+			"error_code",
+			"group",
+			"method",
+			"name",
+			"proto",
+			"scenario",
+			"status",
+			"subproto",
+			"tls_version",
+			"url",
+			"extra_tags",
+		},
+		{
+			"http_reqs",
+			"1595325560",
+			"1.000000",
+			"",
+			"",
+			"",
+			"",
+			"GET",
+			"http://test.k6.io",
+			"HTTP/1.1",
+			"default",
+			"200",
+			"",
+			"",
+			"http://test.k6.io",
+			"",
+		},
+	},
+		result)
+}
+
+func TestWriteCSVFile(t *testing.T) {
+	filename := "../tests/write.csv"
+	data := [][]string{
+		{
+			"header1",
+			"header2",
+		},
+		{
+			"data1",
+			"data2",
+		},
+	}
+
+	writeCSVFile(filename, data)
+
+	file, err := os.Open(filename)
+	assert.Nil(t, err)
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, data, records)
+
+	err = os.Remove(filename)
+	assert.Nil(t, err)
+}
+
+func TestRunCommand(t *testing.T) {
+	filename := "../tests/result.csv"
+	filenameExtracted := "../tests/result-extracted.csv"
+	rootCmd.Run(nil, []string{filename})
+
+	file, err := os.Open(filenameExtracted)
+	assert.Nil(t, err)
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, [][]string{
+		{
+			"timestamp",
+			"duration",
+			"check",
+			"error",
+			"error_code",
+			"group",
+			"method",
+			"name",
+			"proto",
+			"scenario",
+			"status",
+			"subproto",
+			"tls_version",
+			"url",
+			"extra_tags",
+		},
+		{
+			"1595325560",
+			"221.899000",
+			"",
+			"",
+			"",
+			"",
+			"GET",
+			"http://test.k6.io",
+			"HTTP/1.1",
+			"default",
+			"200",
+			"",
+			"",
+			"http://test.k6.io",
+			"",
+		},
+		{
+			"1595325560",
+			"221.899000",
+			"",
+			"",
+			"",
+			"",
+			"GET",
+			"http://test.k6.io",
+			"HTTP/1.1",
+			"default",
+			"200",
+			"",
+			"",
+			"http://test.k6.io",
+			"",
+		},
+	},
+		records)
+
+	err = os.Remove(filenameExtracted)
+	assert.Nil(t, err)
 }
